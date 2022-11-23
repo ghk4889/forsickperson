fde054589d13b22f7e17ef1f15d45743db093eb6
JongDeu <ghk4889@naver.com>
2022-11-23 18:59:42 +0900

    Initial commit

---
+++ src/main/java/com/ojh/forsickperson/ForsickpersonApplication.java
@@ -0,0 +1,13 @@
+package com.ojh.forsickperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForsickpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForsickpersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojh/forsickperson/ForsickpersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojh.forsickperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForsickpersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
